603c6634f48d5e1425ce8555c8b5480635166274
Ghullam Mustafa <gmustafawaince180@gmail.com>
2023-04-27 15:58:39 +0500

    complete

--- script.js
+++ script.js
@@ -16,18 +16,60 @@ function cammelCase() {
     document.getElementById('output').innerHTML= "<h3>Example</h3><ul><li>user</li><li>userResponse</li><li>userResponseTime</li><li>userResponseTimeLimit</li></ul>"
 }
 
-function sunTwo() {
+function sumTwo() {
     document.getElementById('orignalstatements').innerHTML= "<p>let num1=10;</p><p>let num2 = 5;</p><p>Let sum = num1 + num2</p>"
-}
-
-function sum() {
-    let num1=10
+ let num1=10
     let num2 = 5
     var add = num1 + num2
     document.getElementById('output').innerHTML=add
 }
 
-function totalSum() {
-    sunTwo()
-    sum()
+
+
+
+
+function sub() {
+    document.getElementById('orignalstatements').innerHTML= "<p>let num1=10;</p><p>let num2 = 5;</p><p>Let sum = num1 - num2</p>"
+  let num1=10
+    let num2 = 5
+    var subtract = num1 - num2
+    document.getElementById('output').innerHTML=subtract
+}
+
+
+
+
+function multiply() {
+    document.getElementById('orignalstatements').innerHTML= "<p>let num1=10;</p><p>let num2 = 5;</p><p>Let sum = num1 x num2</p>"
+    let num1=10
+    let num2 = 5
+    var multiply = num1 * num2
+    document.getElementById('output').innerHTML=multiply
+}
+
+
+function devide() {
+    document.getElementById('orignalstatements').innerHTML= "<p>let num1=10;</p><p>let num2 = 5;</p><p>Let sum = num1 / num2</p>"
+let num1=10
+    let num2 = 5
+    var devide = num1 / num2
+    document.getElementById('output').innerHTML=devide
+}
+
+
+
+
+
+function someNumbers() {
+    document.getElementById('orignalstatements').innerHTML="36/6*3;"
+    var someNumbers = 36/6*3
+    document.getElementById('output').innerHTML=someNumbers
+}
+
+document.getElementById('clear').onclick =function () {
+    document.getElementById('orignalstatements').innerHTML=''
+}
+
+document.getElementById('clean').onclick =function () {
+    document.getElementById('output').innerHTML=''
 }
